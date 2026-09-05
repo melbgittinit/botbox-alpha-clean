ec5c8d4fbe5c7013f1e49b0176121a6cd65838a8
melbgittinit <melbanks2002@yahoo.com>
2026-09-04 21:03:32 -0400

    Add Creator College persistent work navigation

---
+++ app/creator-college/layout.tsx
@@ -0,0 +1,14 @@
+import "./milestone4.css";
+
+export default function CreatorCollegeLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="cc4-frame">
+      {children}
+      <nav className="cc4-nav" aria-label="Creator College">
+        <a href="/creator-college"><span>⌂</span><b>Campus</b></a>
+        <a href="/creator-college/desk"><span>✦</span><b>Desk</b></a>
+        <a href="/creator-college/locker"><span>▦</span><b>Locker</b></a>
+      </nav>
+    </div>
+  );
+}
