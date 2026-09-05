0e3d009d6976681243111e590268d98d61237f06
melbgittinit <melbanks2002@yahoo.com>
2026-09-04 23:30:22 -0400

    Add Brand Hall to Creator College navigation

--- app/creator-college/layout.tsx
+++ app/creator-college/layout.tsx
@@ -8,6 +8,7 @@ export default function CreatorCollegeLayout({ children }: { children: React.Rea
         <a href="/creator-college"><span>⌂</span><b>Campus</b></a>
         <a href="/creator-college/desk"><span>✦</span><b>Desk</b></a>
         <a href="/creator-college/locker"><span>▦</span><b>Locker</b></a>
+        <a href="/creator-college/brand-hall"><span>◇</span><b>Brands</b></a>
         <a href="/creator-college/profile"><span>◎</span><b>Profile</b></a>
       </nav>
     </div>
