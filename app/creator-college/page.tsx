a5818c0849c3c281763ca43d402a1bc58f1b4f33
melbgittinit <melbanks2002@yahoo.com>
2026-09-04 20:16:32 -0400

    Add Creator College V1 route

---
+++ app/creator-college/page.tsx
@@ -0,0 +1,11 @@
+import "./creator-college.css";
+import CreatorCollegeClient from "./CreatorCollegeClient";
+
+export const metadata = {
+  title: "Creator College | Elevate HUB",
+  description: "Bring an idea. Leave with something made.",
+};
+
+export default function CreatorCollegePage() {
+  return <CreatorCollegeClient />;
+}
